8f21c96bc1d7900f6348f506df985455d3b597b2
Pedro Barbas <pedrodrlvbarbas@gmail.com>
2024-11-19 09:04:21 -0300

    Desenvolvendo App com novos componentes

--- backoffice-rh/src/App.vue
+++ backoffice-rh/src/App.vue
@@ -1,14 +1,28 @@
 <script setup lang="ts">
-import Header from "./components/Header.vue";
 import Aside from "./components/Aside.vue";
+import Card from "./components/Card.vue";
+import Header from "./components/Header.vue";
 </script>
 
 <template>
   <div>
     <Header />
-    <Aside />
+    <div class="main">
+      <Aside />
+      <div class="cards">
+        <Card />
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped>
+.main {
+  display: flex;
+}
+
+.cards { 
+  padding-top: 172px;
+  width: 100%;
+}
 </style>
