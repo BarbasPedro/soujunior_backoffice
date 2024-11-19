8884f8f94b28b71989c3f2242cb7515b372509c4
Pedro Barbas <pedrodrlvbarbas@gmail.com>
2024-11-19 09:01:02 -0300

    Criando o componente Button

---
+++ backoffice-rh/src/components/Button.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+defineProps({
+  placeholder: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+    <button>{{placeholder}}</button>
+  <!-- <label :for="placeholder">
+    <input type="checkbox" :name="placeholder" :id="placeholder" />
+  </label> -->
+</template>
+
+<style scoped>
+</style>
