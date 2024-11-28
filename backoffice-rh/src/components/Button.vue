61be4dfb68b61ecdaf5d75f143cbd541a55e4174
Pedro Barbas <pedrodrlvbarbas@gmail.com>
2024-11-28 19:44:32 -0300

    Adicionando Prop e estilo

--- backoffice-rh/src/components/Button.vue
+++ backoffice-rh/src/components/Button.vue
@@ -4,15 +4,36 @@ defineProps({
     type: String,
     required: true,
   },
+  iconName: {
+    type: String,
+    required: false
+  }
 });
 </script>
 
 <template>
-    <button>{{placeholder}}</button>
-  <!-- <label :for="placeholder">
-    <input type="checkbox" :name="placeholder" :id="placeholder" />
-  </label> -->
+  <div class="button">
+    <img :src="`../../public/${iconName}`" alt="">
+    <p>{{ placeholder }}</p>
+  </div>
 </template>
-
 <style scoped>
+.button {
+  background-color: var(--primary-color);
+  color: var(--secondary-text-color);
+  font-size: 16px;
+  padding: 8px 24px;
+  border-radius: 8px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: fit-content;
+
+
+  img {
+    max-width: 24px;
+    height: 24px;
+    margin-right: 8px;
+  }
+}
 </style>
